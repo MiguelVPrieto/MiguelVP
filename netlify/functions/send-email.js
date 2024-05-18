6237d3f99c0fd4fc2af4080b3797ce63502d7971
MiguelVPrieto <miguelprieto.espanha@gmail.com>
2024-05-18 19:17:36 -0300

    main

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,13 +1,11 @@
 const emailjs = require('emailjs-com');
-const { YOUR_USER_ID, YOUR_SERVICE_ID, YOUR_TEMPLATE_ID } = process.env;
 
 exports.handler = async function(event, context) {
   const data = JSON.parse(event.body);
-
-  emailjs.init(YOUR_USER_ID);
+  emailjs.init(OuOrAaTX1fcs34oH3pnPb);
 
   try {
-    const response = await emailjs.send(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, data);
+    const response = await emailjs.send(service_5mcxryi, template_3wsttqj, data);
     return {
       statusCode: 200,
       body: JSON.stringify({ message: 'Email sent successfully' })
