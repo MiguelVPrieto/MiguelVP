ad7ea6e899a5d7a9981b771e432b54fc7e275a0b
MiguelVPrieto <miguelprieto.espanha@gmail.com>
2024-05-19 16:26:02 -0300

    main

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,19 +1,14 @@
-const emailjs = require('emailjs-com');
+emailjs.init({
+    publicKey: "KEu1x-teiGVB_dZUD",
+});
 
-exports.handler = async function(event, context) {
-  const data = JSON.parse(event.body);
-  emailjs.init(OuOrAaTX1fcs34oH3pnPb);
-
-  try {
-    const response = await emailjs.send(service_5mcxryi, template_3wsttqj, data);
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Email sent successfully' })
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Failed to send email', error })
-    };
+function sendEmail() {
+  var params = {
+    name : document.getElementById("name").value,
+    request : document.getElementById("request").value,
+    email : document.getElementById("email").value
   }
-};
+  emailjs.send("service_5mcxryi", "template_3wsttqj", params).then(function (res) {
+    alert("Success!" + res.status);
+  })
+}
